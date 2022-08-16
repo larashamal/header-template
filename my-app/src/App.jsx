8f40cbbe485e4c188e03a596914ecdfb229733bd
Lara Namiq <larasnamiq@gmail.com>
2022-08-16 19:02:07 +0100

    Fix code

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,18 @@
-import Header from "components/Header";
+import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
+
 
 function App() {
-  return <Header name="John" />;
+  const [name, setName] = useState("Mark");
+
+  return (
+    <>
+      <Header name={name} />
+      <Form />
+    </>
+  );
 }
 
 export default App;
