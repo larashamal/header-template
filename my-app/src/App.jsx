84c95ee1698778e9b03a5d6084705c9601cd961e
Lara Namiq <larasnamiq@gmail.com>
2022-08-18 12:25:16 +0100

    Add products data

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,7 +2,43 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
+import Profile from "./components/Profile/Profile";
 
+const imgUrl = "https://i.imgur.com/yXOvdOSs.jpg";
+const products = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
 
 function App() {
   const [name, setName] = useState("Mark");
@@ -10,7 +46,9 @@ function App() {
   return (
     <>
       <Header name={name} />
-      <Form />
+      <Form setName={setName} />
+      <Profile name={name} imgUrl={imgUrl} />
+      <Products products={products} />
     </>
   );
 }
